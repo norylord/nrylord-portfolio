89c379712dfeb27facf6f5c39b86a2ee8804636c
nrylord <61661058+nrylord@users.noreply.github.com>
2022-08-17 16:00:44 +0300

    added nav links

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import styles from './Navbar.module.sass'
 import FcHome from 'react-icons/fa';
+import Link from "next/link";
 
 const Navbar = () => {
     return (
         <div className={styles.navbar}>
-            <h3 className={styles.navbar__item}>Главная</h3>
-            <h3 className={styles.navbar__item}>Проекты</h3>
+            <Link href='/'><h3 className={styles.navbar__item}>Главная</h3></Link>
+            <Link href='/projects'><h3 className={styles.navbar__item}>Проекты</h3></Link>
             <h3 className={styles.navbar__item}>GitHub</h3>
             <h3 className={styles.navbar__item}>Стек</h3>
         </div>
