6a9454645e1e614f012835f0e73f0899c1f67e1e
nrylord <neoforest3@gmail.com>
2022-10-06 15:10:23 +0300

    123

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import styles from './Navbar.module.sass'
 import FcHome from 'react-icons/fa';
 import Link from "next/link";
+import {useRouter} from "next/router";
 
 const Navbar = () => {
+    const router = useRouter()
     return (
         <div className={styles.navbar}>
-            <Link href='/'><h3 className={styles.navbar__item}>Главная</h3></Link>
-            <Link href='/projects'><h3 className={styles.navbar__item}>Проекты</h3></Link>
+            <Link href='/'><h3 className={`${styles.navbar__item} ${router.asPath === '/' ? styles.active : null}`}>Главная</h3></Link>
+            <Link href='/projects'><h3 className={`${styles.navbar__item} ${router.asPath === '/projects' ? styles.active : null}`}>Проекты</h3></Link>
             <a className={styles.navbar__item} target="_blank" href="https://github.com/nrylord"
                rel="noopener noreferrer"><h3
                 style={{fontWeight: 300}}>GitHub</h3></a>
